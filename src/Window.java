8c8192314f269a834149b70cbe9c73907e7e9f02
bernardoffer <50500849+bernardoffer@users.noreply.github.com>
2022-12-13 18:40:24 -0300

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -13,11 +13,11 @@ class Window extends JFrame{
 	public Window(){
 		
 		
-		// Creates the arraylist that'll contain the threads
+
 		Grid = new ArrayList<ArrayList<DataOfSquare>>();
 		ArrayList<DataOfSquare> data;
 		
-		// Creates Threads and its data and adds it to the arrayList
+
 		for(int i=0;i<width;i++){
 			data= new ArrayList<DataOfSquare>();
 			for(int j=0;j<height;j++){
@@ -27,31 +27,28 @@ class Window extends JFrame{
 			Grid.add(data);
 		}
 		
-		// Setting up the layout of the panel
+
 		getContentPane().setLayout(new GridLayout(20,20,0,0));
 		
-		// Start & pauses all threads, then adds every square of each thread to the panel
+
 		for(int i=0;i<width;i++){
 			for(int j=0;j<height;j++){
 				getContentPane().add(Grid.get(i).get(j).square);
 			}
 		}
 		
-		// initial position of the snake
+
 		Tuple position = new Tuple(10,10);
 		// passing this value to the controller
 		ThreadsController c = new ThreadsController(position);
 		//Let's start the game now..
 		c.start();
 
-		// Links the window to the keyboardlistenner.
 		this.addKeyListener((KeyListener) new KeyboardListener());
 
-		//To do : handle multiplayers .. The above works, test it and see what happens
+
 		
-		//Tuple position2 = new Tuple(13,13);
-		//ControlleurThreads c2 = new ControlleurThreads(position2);
-		//c2.start();
+
 		
 	}
 }
