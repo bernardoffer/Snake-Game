09930cb8ad37c1286dc1d263042481227783accb
bernardoffer <50500849+bernardoffer@users.noreply.github.com>
2022-12-13 18:37:30 -0300

    Update DataOfSquare.java

--- src/DataOfSquare.java
+++ src/DataOfSquare.java
@@ -4,13 +4,13 @@ import java.awt.Color;
 public class DataOfSquare {
 
 	
-	//ArrayList that'll contain the colors
+	
 	ArrayList<Color> C =new ArrayList<Color>();
 	int color; //2: snake , 1: food, 0:empty 
 	SquarePanel square;
 	public DataOfSquare(int col){
 		
-		//Lets add the color to the arrayList
+		
 		C.add(Color.darkGray);//0
 		C.add(Color.BLUE);    //1
 		C.add(Color.white);   //2
