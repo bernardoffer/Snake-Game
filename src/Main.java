b5fb476843be467d294e9ab80a6ccad4e151b65a
bernardoffer <50500849+bernardoffer@users.noreply.github.com>
2022-12-13 18:38:15 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//Creating the window with all its awesome snaky features
+		
 		Window f1= new Window();
 		
-		//Setting up the window settings
+		
 		f1.setTitle("Snake");
 		f1.setSize(300,300);
 		f1.setVisible(true);
